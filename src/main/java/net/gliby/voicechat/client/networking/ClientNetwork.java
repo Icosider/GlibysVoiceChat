db1c49bc42fe346cac59461e538daa2a8bdd9b51
Jacob Farley <jacobfarley2017@gmail.com>
2019-07-20 16:22:20 -0500

    Fix UDP not working due to a screwed up switch statement.

--- src/main/java/net/gliby/voicechat/client/networking/ClientNetwork.java
+++ src/main/java/net/gliby/voicechat/client/networking/ClientNetwork.java
@@ -64,11 +64,11 @@ public class ClientNetwork {
 
       VoiceChatClient.getSoundManager().reset();
 
-      switch (type.ordinal()) {
-         case 1:
+      switch (type) {
+         case MINECRAFT:
             this.voiceClient = new MinecraftVoiceClient(type);
             break;
-         case 2:
+         case UDP:
             String serverAddress = ip;
             if (ip.isEmpty()) {
                ServerData serverData;
